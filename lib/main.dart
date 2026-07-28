b3e06b526a4a1fd8054c1dec5556691ac15856bd
Anonymous_N <nam348tnh.3gp@gmail.com>
2026-07-28 21:30:44 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 import 'home_screen.dart';
+import 'miner_task_handler.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  FlutterForegroundTask.init(
+    androidNotificationOptions: AndroidNotificationOptions(
+      channelId: 'miner_channel',
+      channelName: 'Duino Miner',
+      channelDescription: 'Keep mining running in background',
+      channelImportance: NotificationChannelImportance.LOW,
+      priority: NotificationPriority.LOW,
+      iconData: const NotificationIconData(
+        resType: ResourceType.mipmap,
+        resPrefix: ResourcePrefix.ic,
+        name: 'launcher',
+      ),
+    ),
+    iosNotificationOptions: const IOSNotificationOptions(
+      showNotification: false,
+    ),
+    foregroundTaskOptions: const ForegroundTaskOptions(
+      autoRunOnBoot: false,
+      allowWakeLock: true,
+      allowWifiLock: false,
+    ),
+  );
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
